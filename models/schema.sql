cfc718b6250e351f6fc8068197b5f72deba4e090
shadowlaq <escamillaa@email.arizona.edu>
2018-08-11 12:05:34 -0700

    Barbara edits

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,33 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
-
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+-- delete database if it exists
+DROP DATABASE IF EXISTS titato;
+-- create and use the database
+CREATE DATABASE titato;
+USE titato;
+-- create players table
+CREATE TABLE players (
+  pid INT NOT NULL AUTO_INCREMENT,
+  playername VARCHAR(100) NOT NULL,
+  PRIMARY KEY (pid)
+);
+-- create gamestats table
+CREATE TABLE gamestats (
+  gsid INT NOT NULL AUTO_INCREMENT,
+  gid INT NOT NULL,
+  pid INT NOT NULL,
+  cel1 VARCHAR(30) NOT NULL,
+  cel2 VARCHAR(30) NOT NULL,
+  cel3 VARCHAR(30) NOT NULL,
+  cel4 VARCHAR(30) NOT NULL,
+  cel5 VARCHAR(30) NOT NULL,
+  cel6 VARCHAR(30) NOT NULL,
+  cel7 VARCHAR(30) NOT NULL,
+  cel8 VARCHAR(30) NOT NULL,
+  cel9 VARCHAR(30) NOT NULL,
+  PRIMARY KEY (gsid)
+);
+-- create games table
+CREATE TABLE games (
+  gid INT NOT NULL AUTO_INCREMENT,
+  gametitle VARCHAR(100) NOT NULL,
+  PRIMARY KEY (gid)
+);
