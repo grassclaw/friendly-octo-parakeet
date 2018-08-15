3ca706e2e323928ac9229c6b7e89740162009d12
shadowlaq <escamillaa@email.arizona.edu>
2018-08-14 20:54:39 -0700

    only states now

--- models/games.js
+++ models/games.js
@@ -1,5 +1,5 @@
 module.exports = function(sequelize, DataTypes) {
-  var Game = sequelize.define("Example", {
+  var Game = sequelize.define("Game", {
     gametitle: {
       type: DataTypes.STRING,
       allowNull: false,
@@ -12,17 +12,17 @@ module.exports = function(sequelize, DataTypes) {
     pid2: DataTypes.NUMBER
   });
 
-  Game.associate = function(models) {
-    // We're saying that a Game should belong to an initial Player who created it
-    // A Game can't be created without an Player1 due to the foreign key constraint
-    Game.belongsTo(models.Players, {
-      foreignKey: {
-        allowNull: false
-      }
-    });
-    Game.hasMany(models.gameState, {
-      // onDelete: "cascade"
-    });
-  };
+  // Game.associate = function(models) {
+  //   // We're saying that a Game should belong to an initial Player who created it
+  //   // A Game can't be created without an Player1 due to the foreign key constraint
+  //   Game.belongsToMany(models.Players, {
+  //     foreignKey: {
+  //       allowNull: false
+  //     }
+  //   });
+  //   Game.hasMany(models.gameState, {
+  //     // onDelete: "cascade"
+  //   });
+  // };
   return Game;
 };
